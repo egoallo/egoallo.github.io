a407b74fbe0edbcbc2b5aeb47cc2a10cfff587f6
brentyi <yibrenth@gmail.com>
2024-10-06 04:26:33 -0700

    Initial commit

---
+++ results.js
@@ -0,0 +1,241 @@
+const results = [
+  [
+    "Coffee Machine",
+    [
+      "/viser-embed/",
+      "?playbackPath=/splash-results/feb28_coffeemachine.viser",
+      "&synchronizedVideoOverlay=/splash-results/feb28_coffeemachine.mp4",
+      "&synchronizedVideoTimeOffset=-0.05",
+      "&initialCameraPosition=-0.27,-0.48,1.21",
+      "&initialCameraLookAt=-0.635,-0.907,-0.703",
+      "&baseSpeed=1.5",
+      "&darkMode",
+    ],
+    "./splash-results/feb28_coffeemachine_thumbnail.jpg",
+  ],
+  [
+    "Jog",
+    [
+      "/viser-embed/",
+      "?playbackPath=/splash-results/feb28_jog.viser",
+      "&synchronizedVideoOverlay=/splash-results/feb28_jog.mp4",
+      "&initialCameraPosition=0.98,8.4,0.58",
+      "&initialCameraLookAt=-1.15,6.68,-0.52",
+      "&darkMode",
+    ],
+    "./splash-results/feb28_jog_thumbnail.jpg",
+  ],
+  // START: Add new results here.
+  [
+    "Coke",
+    [
+      "/viser-embed/",
+      "?playbackPath=/splash-results/coke.viser",
+      "&synchronizedVideoOverlay=/splash-results/coke.mp4",
+      "&initialCameraPosition=-0.41,0.26,1.31",
+      "&initialCameraLookAt=0.84,-1.04,-0.73",
+      "&baseSpeed=1.5",
+      "&darkMode",
+    ],
+    "./splash-results/coke_thumbnail.jpg",
+  ],
+  [
+    "Interactions",
+    [
+      "/viser-embed/",
+      "?playbackPath=/splash-results/feb13_interactions_4.viser",
+      "&synchronizedVideoOverlay=/splash-results/feb13_interactions_4.mp4",
+      //[0.2997831938375523, 2.1342957383026113, 0.34981093272597924] Vector3 {x: -0.6031762534566718, y: 0.7336576301086007, z: -0.5246216717166879}
+      "&initialCameraPosition=0.30,2.13,0.35",
+      "&initialCameraLookAt=-0.60,0.73,-0.52",
+      "&baseSpeed=1.5",
+      "&darkMode",
+    ],
+    "./splash-results/feb13_interactions_4_thumbnail.jpg",
+  ],
+  [
+    "Georgia Tech Bike",
+    [
+      "/viser-embed/",
+      "?playbackPath=/splash-results/georgiatech_bike_14_6.viser",
+      "&synchronizedVideoOverlay=/splash-results/georgiatech_bike_14_6.mp4",
+      // [-0.396219835130477, -1.1357444926752176, 0.9790719735463433] Vector3 {x: -1.0039545078882994, y: -0.08309590420004448, z: -0.4766981811277936}
+      "&initialCameraPosition=-0.40,-1.14,0.98",
+      "&initialCameraLookAt=-1.00,-0.08,-0.48",
+      "&baseSpeed=2",
+      "&darkMode",
+    ],
+    "./splash-results/georgiatech_bike_14_6_thumbnail.jpg",
+  ],
+  [
+    "Georgia Tech Covid",
+    [
+      "/viser-embed/",
+      "?playbackPath=/splash-results/georgiatech_covid_06_4.viser",
+      "&synchronizedVideoOverlay=/splash-results/georgiatech_covid_06_4.mp4",
+      //  [4.223715689629469, 6.4951232492422335, 6.773491930389971] Vector3 {x: 4.147565348998104, y: 5.340959239743921, z: 6.034096702898773}
+      "&initialCameraPosition=4.22,6.50,6.77",
+      "&initialCameraLookAt=4.15,5.34,6.03",
+      "&baseSpeed=2",
+      "&darkMode",
+    ],
+    "./splash-results/georgiatech_covid_06_4_thumbnail.jpg",
+  ],
+  [
+    "IIITH Cooking 120",
+    [
+      "/viser-embed/",
+      "?playbackPath=/splash-results/iiith_cooking_120_2.viser",
+      "&synchronizedVideoOverlay=/splash-results/iiith_cooking_120_2.mp4",
+      // [0.3975282978506596, -1.0932541698979226, 1.1602779942840218] Vector3 {x: -0.8826991649205477, y: -0.6619435915962723, z: -0.5110887872398087}
+      "&initialCameraPosition=0.40,-1.09,1.16",
+      "&initialCameraLookAt=-0.88,-0.66,-0.51",
+      "&baseSpeed=2",
+      "&darkMode",
+    ],
+    "./splash-results/iiith_cooking_120_2_thumbnail.jpg",
+  ],
+  [
+    "IIITH Cooking 64",
+    [
+      "/viser-embed/",
+      "?playbackPath=/splash-results/iiith_cooking_64_2.viser",
+      "&synchronizedVideoOverlay=/splash-results/iiith_cooking_64_2.mp4",
+      //[0.05965581411395848, -1.1239165240150137, 0.6423309058657197] Vector3 {x: -0.6958953557159605, y: -0.42701500354541533, z: -0.4142444297194353}
+      "&initialCameraPosition=0.06,-1.12,0.64",
+      "&initialCameraLookAt=-0.70,-0.43,-0.41",
+      "&baseSpeed=2",
+      "&darkMode",
+    ],
+    "./splash-results/iiith_cooking_64_2_thumbnail.jpg",
+  ],
+  [
+    "UPenn Piano",
+    [
+      "/viser-embed/",
+      "?playbackPath=/splash-results/upenn_0710_Piano_1_3.viser",
+      "&synchronizedVideoOverlay=/splash-results/upenn_0710_Piano_1_3.mp4",
+      //[2.5318685083677357, -0.42315967880890853, 1.2715837709981566] Vector3 {x: 0.11922650471882609, y: -0.5489573474719631, z: -0.5803120059202549}
+      "&initialCameraPosition=2.53,-0.42,1.27",
+      "&initialCameraLookAt=0.12,-0.55,-0.58",
+      "&baseSpeed=2",
+      "&darkMode",
+    ],
+    "./splash-results/upenn_0710_Piano_1_3_thumbnail.jpg",
+  ],
+];
+
+function initializeResultSelector(resultsElement) {
+  const selectorElement = resultsElement.querySelector(".results-selector");
+  const resultsThumbnails = selectorElement.querySelector(
+    ".results-thumbnails",
+  );
+  const prevButton = selectorElement.querySelector(".results-prev");
+  const nextButton = selectorElement.querySelector(".results-next");
+  let currentIndex = 0;
+
+  function createIframe(src) {
+    const iframe = document.createElement("iframe");
+    console.log("Creating iframe with src", src);
+    iframe.src = src;
+    return iframe;
+  }
+
+  function showIframe(src) {
+    const wrapper = resultsElement.querySelector(".iframe-wrapper");
+    wrapper.innerHTML = "";
+    const iframe = createIframe(Array.isArray(src) ? src.join("") : src);
+    wrapper.appendChild(iframe);
+  }
+
+  function updateSelection(index) {
+    currentIndex = index;
+    resultsThumbnails
+      .querySelectorAll("a")
+      .forEach((a, i) =>
+        a.setAttribute("data-selected", i === index ? "true" : "false"),
+      );
+    showIframe(results[index][1]);
+    const selectedThumbnail = resultsThumbnails.children[index];
+
+    // Scroll the selected thumbnail into view
+    const thumbnailsContainer = resultsThumbnails;
+    const scrollLeft =
+      selectedThumbnail.offsetLeft -
+      (thumbnailsContainer.clientWidth - selectedThumbnail.clientWidth) / 2;
+    thumbnailsContainer.scrollTo({
+      left: scrollLeft,
+      behavior: "smooth",
+    });
+
+    // Update URL with the selected result
+    const resultName = results[index][0].toLowerCase().replace(/\s+/g, "-");
+    history.pushState(null, "", `?result=${resultName}`);
+  }
+
+  results.forEach(([label, src, thumbnail], index) => {
+    const link = document.createElement("a");
+    link.href = "#";
+    link.setAttribute("data-selected", index === 0 ? "true" : "false");
+    link.addEventListener("click", (e) => {
+      e.preventDefault();
+      updateSelection(index);
+    });
+
+    const img = document.createElement("img");
+    img.src = thumbnail;
+    img.alt =
+      "Thumbnail that can be clicked to show a result of our method: " + label;
+    img.title = label;
+
+    link.appendChild(img);
+    resultsThumbnails.appendChild(link);
+  });
+
+  prevButton.addEventListener("click", () => {
+    updateSelection((currentIndex - 1 + results.length) % results.length);
+  });
+
+  nextButton.addEventListener("click", () => {
+    updateSelection((currentIndex + 1) % results.length);
+  });
+
+  // Check URL for initial result selection
+  const urlParams = new URLSearchParams(window.location.search);
+  const initialResult = urlParams.get("result");
+  if (initialResult) {
+    const index = results.findIndex(
+      (result) =>
+        result[0].toLowerCase().replace(/\s+/g, "-") === initialResult,
+    );
+    if (index !== -1) {
+      updateSelection(index);
+    } else {
+      showIframe(results[0][1]);
+    }
+  } else {
+    showIframe(results[0][1]);
+  }
+}
+
+// Initialize all result on the page
+document.querySelectorAll(".results").forEach(initializeResultSelector);
+//
+// [
+//   "Dance (EgoExo)",
+//   [
+//     "https://brentyi.github.io/egoalgo-results/build/",
+//     "?playbackPath=https://brentyi.github.io/egoalgo-results/july17/egoexo_0.viser",
+//     "&darkMode",
+//   ],
+//   "./results_thumbnails/coffee.png",
+// ],
+// [
+//   "Basketball (EgoExo)",
+//   [
+//     "https://brentyi.github.io/egoalgo-results/build/",
+//     "?playbackPath=https://brentyi.github.io/egoalgo-results/july17/egoexo_4.viser",
+//     "&darkMode",
+//   ],
+//   "./results_thumbnails/coffee.png",
+// ],
